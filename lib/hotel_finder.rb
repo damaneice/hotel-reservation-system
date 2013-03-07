5a364b77690878c46d3518ed06b0028ec3fd32cc
Damon Maneice <Thoughtworker@DM8998.local>
2013-03-07 12:38:15 -0300

    added logic to find highest rated hotel when cost is the same

--- lib/hotel_finder.rb
+++ lib/hotel_finder.rb
@@ -4,6 +4,23 @@ class HotelFinder
 		@hotels = hotels
 	end
 	def findCheapestHotel customerType, dates
-		@hotels.sort_by {|i| i.total(customerType, dates)}
+		hotels = @hotels.sort_by {|i| i.total(customerType, dates)}
+		cheapestPrice = hotels.first.total(customerType, dates)
+		hotelsSamePrice = []
+
+		hotels.each do |hotel|
+			if cheapestPrice == hotel.total(customerType, dates) 
+				hotelsSamePrice.push(hotel)
+			end
+		end
+		
+		return hotels.first if (hotelsSamePrice.length == 1)
+		return findHighestRated(hotelsSamePrice)
+	end
+
+	private 
+	def findHighestRated hotels
+		hotels = hotels.sort {|a,b| b.rating <=> a.rating}	
+		return hotels.first
 	end
 end
--- spec/hotel_finder_spec.rb
+++ spec/hotel_finder_spec.rb
@@ -13,7 +13,59 @@ describe HotelFinder do
         hotels = [lakewood, bridgewood]
 
         hotelFinder = HotelFinder.new(hotels)
-        hotelFinder.findCheapestHotel(:rewards, dates).first.name.should eq "bridgewood" 
+        hotelFinder.findCheapestHotel(:rewards, dates).name.should eq "bridgewood" 
   		end
+
+        it "should be lakewood hotel" do
+        @lakewood =  {:name => "lakewood", :rating => 3, :weekedayPrice => 110, :weekendPrice => 80, :rewardsWeekdayPrice => 80, :rewardsWeekendPrice => 60 }
+        @bridgewood =  {:name => "bridgewood", :rating => 4, :weekedayPrice => 160, :weekendPrice => 110, :rewardsWeekdayPrice => 120, :rewardsWeekendPrice => 50 }
+        dates = [Date.parse('14Mar2009(sat)'), Date.parse('15Mar2009(sun)'), Date.parse('16Mar2009(mon)')]
+        lakewood = Hotel.new(@lakewood)
+        bridgewood = Hotel.new(@bridgewood)
+        hotels = [lakewood, bridgewood]
+
+        hotelFinder = HotelFinder.new(hotels)
+        hotelFinder.findCheapestHotel(:rewards, dates).name.should eq "lakewood" 
+      end
   	end
+
+    describe 'regular customer' do
+      it "should be bridgewood hotel" do
+        @lakewood =  {:name => "lakewood", :rating => 3, :weekedayPrice => 110, :weekendPrice => 80, :rewardsWeekdayPrice => 90, :rewardsWeekendPrice => 70 }
+        @bridgewood =  {:name => "bridgewood", :rating => 4, :weekedayPrice => 160, :weekendPrice => 110, :rewardsWeekdayPrice => 60, :rewardsWeekendPrice => 50 }
+        dates = [Date.parse('14Mar2009(sat)')]
+        lakewood = Hotel.new(@lakewood)
+        bridgewood = Hotel.new(@bridgewood)
+        hotels = [lakewood, bridgewood]
+
+        hotelFinder = HotelFinder.new(hotels)
+        hotelFinder.findCheapestHotel(:regular, dates).name.should eq "lakewood" 
+      end
+
+        it "should be lakewood hotel" do
+        @lakewood =  {:name => "lakewood", :rating => 3, :weekedayPrice => 110, :weekendPrice => 80, :rewardsWeekdayPrice => 80, :rewardsWeekendPrice => 60 }
+        @bridgewood =  {:name => "bridgewood", :rating => 4, :weekedayPrice => 160, :weekendPrice => 110, :rewardsWeekdayPrice => 120, :rewardsWeekendPrice => 50 }
+        dates = [Date.parse('14Mar2009(sat)'), Date.parse('15Mar2009(sun)'), Date.parse('16Mar2009(mon)')]
+        lakewood = Hotel.new(@lakewood)
+        bridgewood = Hotel.new(@bridgewood)
+        hotels = [lakewood, bridgewood]
+
+        hotelFinder = HotelFinder.new(hotels)
+        hotelFinder.findCheapestHotel(:regular, dates).name.should eq "lakewood" 
+      end
+    end
+
+     describe 'same price' do
+      it "should be highest rated hotel" do
+        @lakewood =  {:name => "lakewood", :rating => 3, :weekedayPrice => 110, :weekendPrice => 80, :rewardsWeekdayPrice => 90, :rewardsWeekendPrice => 70 }
+        @bridgewood =  {:name => "bridgewood", :rating => 4, :weekedayPrice => 110, :weekendPrice => 110, :rewardsWeekdayPrice => 60, :rewardsWeekendPrice => 50 }
+        dates = [Date.parse('16Mar2009(mon)')]
+        lakewood = Hotel.new(@lakewood)
+        bridgewood = Hotel.new(@bridgewood)
+        hotels = [lakewood, bridgewood]
+
+        hotelFinder = HotelFinder.new(hotels)
+        hotelFinder.findCheapestHotel(:regular, dates).name.should eq "bridgewood" 
+      end
+    end
   end
